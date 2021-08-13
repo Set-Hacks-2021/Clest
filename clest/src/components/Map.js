85a5db36a9ac6166d3626ff528bf97cce2add9bc
Hackyeahh <henry.xiu@gx.cds.on.ca>
2021-08-13 17:38:32 -0400

    im going to take a power nap
    bye
    
    Co-Authored-By: Indom1nus <66338057+Indom1nus@users.noreply.github.com>
    Co-Authored-By: Shervin the Prodigy <68794773+Shervi28@users.noreply.github.com>
    Co-Authored-By: Arxshot <66337683+Arxshot@users.noreply.github.com>

--- clest/src/components/Map.js
+++ clest/src/components/Map.js
@@ -2,38 +2,57 @@ import React from 'react'
 import GoogleMapReact from 'google-map-react'
 import "../styles/map.scss";
 
+fetchAQIdata();
+
 const location = {
     address: '1600 Amphitheatre Parkway, Mountain View, california.',
     lat: 37.42216,
     lng: -122.08427,
+}
+
+
+async function fetchAQIdata(){
+  const key = 'e92a140d7bcfad9ccb8bd5fb9d3a1f532bcb88e8';
+
+  async function getDataFromCity(city){
+    const response = fetch(`https://api.waqi.info/feed/${city}/?token=${key}`, {
+      method: 'GET'
+    })
+
+    return (await response).json()
   }
-
-
-  const LocationPin = ({ text }) => (
-    <div className="pin">
-      {/* <Icon icon={locationIcon} className="pin-icon" /> */}
-      <p className="pin-text">{text}</p>
-    </div>
-  )
-
-  const Map = ({ location, zoomLevel }) => (
-    <div className="map">
-      <h2 className="map-h2">Come Visit Us At Our Campus</h2>
   
-      <div className="google-map">
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: '' }}
-          defaultCenter={location}
-          defaultZoom={zoomLevel}
-        >
-          <LocationPin
-            lat={location.lat}
-            lng={location.lng}
-            text={location.address}
-          />
-        </GoogleMapReact>
-      </div>
+  console.log('i ran')
+  console.log(await getDataFromCity('shanghai'));
+}
+
+const LocationPin = ({ text }) => (
+  <div className="pin">
+    {/* <Icon icon={locationIcon} className="pin-icon" /> */}
+    <p className="pin-text">{text}</p>
+  </div>
+)
+
+const Map = ({ location, zoomLevel }) => (
+  <div className="map">
+    <h2 className="map-h2">Come Visit Us At Our Campus</h2>
+
+
+    <p></p>
+    <div className="google-map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyDJNmdmk9BmJxd6tLolaugUhmo7l5ou_4U' }}
+        defaultCenter={location}
+        defaultZoom={zoomLevel}
+      >
+        <LocationPin
+          lat={location.lat}
+          lng={location.lng}
+          text={location.address}
+        />
+      </GoogleMapReact>
     </div>
-  )
+  </div>
+)
 
 export default Map;
--- clest/src/pages/homePage.js
+++ clest/src/pages/homePage.js
@@ -1,9 +1,55 @@
 import '../styles/styles.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+import Button from 'react-bootstrap/Button';
+//var voronoi = require('@turf/voronoi');
+import voronoi from '@turf/voronoi';
+import * as turf from '@turf/turf';
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDfzPF3G7jy5Ha6clqJbIBB-sIXwJuHiw4",
+    authDomain: "clest-322818.firebaseapp.com",
+    projectId: "clest-322818",
+    storageBucket: "clest-322818.appspot.com",
+    messagingSenderId: "860346307090",
+    appId: "1:860346307090:web:2e6dfb6d96a414abd13ecb",
+    measurementId: "G-TH9CPDXJ1X"
+};
+
+if (!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+}else{
+    firebase.app();
+}
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
 
 function HomePage(){
     return (
         <section className='page'>
             <Landing />
+        </section>
+    );
+}
+
+function Landing(){
+    return (
+        <section className='landing'>
+            <h1>Clest</h1>
+            <h2>Improve your community, one quest at a time.</h2>
+            <Login />
+            <Testing />
         </section>
     );
 }
@@ -23,20 +69,42 @@ function Login(){
     //   // ...
     // });
 
-    return (
+    const [user] = useAuthState(auth);
 
-        <button>Sign in with Google.</button>
+
+    const signInWithGoogle = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider);
+    }
+
+    return (
+        // if user is null, then display login button 
+        <div>
+            { user ? 
+            <Button href='/map'>go to map {user.email}</Button>  // runs if user signed in
+            : 
+            <Button onClick={signInWithGoogle}>Join with Google.</Button> // runs if user not signed in
+            }
+        </div>
     );
   
   }
 
-function Landing(){
-    return (
-        <section>
-            <Login />
-        </section>
-    );
-}
+  function Testing()
+  {
+    var options = {
+        bbox: [-70, 40, -60, 60]
+      };
+      var points = turf.randomPoint(100, options);
+      var voronoiPolygons = turf.voronoi(points, options);
+      
+      return(
+          voronoiPolygons
+      )
+  }
+
+
+
 
 
 
